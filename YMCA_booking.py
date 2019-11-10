3b20579e83462f90900ffca5adbf73812702f2c9
Yang Ding <dyt811@gmail.com>
2019-11-10 14:10:05 -0500

    Travis-CI Debug remove buggy Chrome arguments

--- YMCA_booking.py
+++ YMCA_booking.py
@@ -87,9 +87,9 @@ class AutomateBooking:
     def PrepareTravisDriver(self):
         chrome_options = Options()
         chrome_options.binary_location = "/usr/bin/google-chrome-stable"
-        chrome_options.add_argument('--no-sandbox')
+        #chrome_options.add_argument('--no-sandbox')
         chrome_options.add_argument('--headless')
-        chrome_options.add_argument('--disable-dev-shm-usage')
+        #chrome_options.add_argument('--disable-dev-shm-usage')
         self.driver = webdriver.Chrome(chrome_options=chrome_options)
 
     def PrepareChromeDriver(self):
